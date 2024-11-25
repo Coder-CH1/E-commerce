6d0df2ce9409c069a3aecb52675e12c9da99402a
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2024-11-25 19:32:39 +0100

    added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:ecommerce/reusable_widgets.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_splash_screen/flutter_splash_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
