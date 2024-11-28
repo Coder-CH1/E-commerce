accc8baac568f1a1e64526e79ca6108e437c2648
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2024-11-28 14:15:34 +0100

    added

--- lib/firebase/database_manager/database.dart
+++ lib/firebase/database_manager/database.dart
@@ -1,11 +1,16 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import '../model/user.dart';
 
 class DatabaseManager {
-  FirebaseFirestore db = FirebaseFirestore.instance;
-
-/// READ DATA
-
-/// UPDATE DATA
-
-/// DELETE DATA
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  Future createUserInFirestore(User user) async {
+    DocumentSnapshot doc = await _firestore.collection('users').doc(user.uid).get();
+    if (!doc.exists) {
+      _firestore.collection('users').doc(user.uid).set({
+        'user': user.user,
+        'email': user.email,
+        'createdOn': DateTime.now(),
+      });
+    }
+  }
 }
---
+++ lib/firebase/model/user.dart
@@ -0,0 +1,9 @@
+
+class User {
+ final String uid;
+final String user;
+final String email;
+final DateTime createOn;
+
+ User({required this.uid,required this.user,required this.email,required this.createOn});
+}
