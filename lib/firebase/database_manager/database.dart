9bfd204aa37110b8e9f70450e66bf3debde241da
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2024-11-28 14:56:19 +0100

    added

--- lib/firebase/database_manager/database.dart
+++ lib/firebase/database_manager/database.dart
@@ -4,13 +4,19 @@ import '../model/user.dart';
 class DatabaseManager {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   Future createUserInFirestore(User user) async {
-    DocumentSnapshot doc = await _firestore.collection('users').doc(user.uid).get();
-    if (!doc.exists) {
-      _firestore.collection('users').doc(user.uid).set({
-        'user': user.user,
-        'email': user.email,
-        'createdOn': DateTime.now(),
-      });
+    try {
+      DocumentSnapshot doc = await _firestore.collection('users')
+          .doc(user.uid)
+          .get();
+      if (!doc.exists) {
+        _firestore.collection('users').doc(user.uid).set({
+          'user': user.user,
+          'email': user.email,
+          'createdOn': DateTime.now(),
+        });
+      }
+    } catch (e) {
+      throw Exception('Error in creating user $e');
     }
   }
 }
