1c0d8c96e76c43c1933baf41828a74ba47ac3770
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2024-12-02 13:37:33 +0100

    added

--- lib/ui/login.dart
+++ lib/ui/login.dart
@@ -8,6 +8,10 @@ class Login extends StatefulWidget {
   State<Login> createState() => _LoginState();
 }
 class _LoginState extends State<Login> {
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,31 +20,40 @@ class _LoginState extends State<Login> {
       ),
       body: Padding(
         padding: const EdgeInsets.only(top: 50, left: 20, right: 20),
-        child: Column(
-          children: [
-            const CustomTextField(
-              labelText: 'email',
-            ),
-            const SizedBox(height: 20),
-            const CustomTextField(
-              labelText: 'password',
-            ),
-            const SizedBox(height: 20),
-            CustomTextButton(
-              text: 'Forgot your password?',
-              onPressed: (){},
-            ),
-            const SizedBox(height: 20),
-            CustomButton(
-              text: 'LOGIN', buttonTextStyle: const TextStyle(color: whiteColor),
-              onPressed: (){},
-              color: redColor,
-              side: BorderSide.none,
-              buttonHeight: 50,
-              buttonWidth: MediaQuery.of(context).size.width,
-              borderRadius: 25,
-            )
-          ],
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const CustomTextField(
+                labelText: 'email',
+              ),
+              const SizedBox(height: 20),
+              const CustomTextField(
+                labelText: 'password',
+              ),
+              const SizedBox(height: 20),
+              CustomTextButton(
+                text: 'Forgot your password?',
+                onPressed: (){},
+              ),
+              const SizedBox(height: 20),
+              Expanded(
+              child:  Align(
+                alignment: Alignment.bottomCenter,
+                child: CustomButton(
+                  text: 'LOGIN', buttonTextStyle: const TextStyle(color: whiteColor),
+                  onPressed: (){},
+                  color: redColor,
+                  side: BorderSide.none,
+                  buttonHeight: 50,
+                  buttonWidth: MediaQuery.of(context).size.width,
+                  borderRadius: 25,
+                ),
+              )
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/ui/signup.dart
+++ lib/ui/signup.dart
@@ -8,37 +8,48 @@ class Signup extends StatefulWidget {
   State<Signup> createState() => _SignupState();
 }
 class _SignupState extends State<Signup> {
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const CustomAppBar(title: 'Sign up'),
       body: Padding(
         padding: const EdgeInsets.only(top: 50, left: 20, right: 20),
-        child: Column(
-          children: [
-            const CustomTextField(
-              labelText: 'email',
-            ),
-            const SizedBox(height: 20),
-            const CustomTextField(
-              labelText: 'password',
-            ),
-            const SizedBox(height: 20),
-            CustomTextButton(
-                text: 'Already have an account?',
-                onPressed: (){},
-            ),
-            const SizedBox(height: 20),
-            CustomButton(
-              text: 'LOGIN', buttonTextStyle: const TextStyle(color: whiteColor),
-                onPressed: (){},
-                color: redColor,
-              side: BorderSide.none,
-              buttonHeight: 50,
-              buttonWidth: MediaQuery.of(context).size.width,
-              borderRadius: 25,
-            )
-          ],
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              const CustomTextField(
+                labelText: 'email',
+              ),
+              const SizedBox(height: 20),
+              const CustomTextField(
+                labelText: 'password',
+              ),
+              const SizedBox(height: 20),
+              CustomTextButton(
+                  text: 'Already have an account?',
+                  onPressed: (){},
+              ),
+              const SizedBox(height: 20),
+              Expanded(
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                child: CustomButton(
+                  text: 'LOGIN', buttonTextStyle: const TextStyle(color: whiteColor),
+                    onPressed: (){},
+                    color: redColor,
+                  side: BorderSide.none,
+                  buttonHeight: 50,
+                  buttonWidth: MediaQuery.of(context).size.width,
+                  borderRadius: 25,
+                ),
+              )
+              )
+            ],
+          ),
         ),
       ),
     );
