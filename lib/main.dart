e9c3a5bdbe4038f02fa81794ce04666777696f58
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2024-12-05 13:08:15 +0100

    added

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,14 @@ import 'package:animated_text_kit/animated_text_kit.dart';
 /// INITIALIZE FIREBASE
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  try {
-    await dotenv.load(fileName: ".env"); // Load environment variables
-  } catch (e) {
-    throw Exception('Error loading .env file: $e'); // Print error if any
-  }
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: dotenv.env['FIREBASE_API_KEY']!,
+      appId: dotenv.env['FIREBASE_APP_ID']!,
+      messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID']!,
+      projectId: dotenv.env['FIREBASE_PROJECT_ID']!,),
+
+  );
   runApp(const MyApp());
 }
 
