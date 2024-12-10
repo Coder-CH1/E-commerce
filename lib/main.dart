6194eaf91a29de6f71d18775e63d6affa399af0b
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2024-12-10 12:28:59 +0100

    added

--- lib/main.dart
+++ lib/main.dart
@@ -3,34 +3,20 @@ import 'package:ecommerce/custom_widgets/custom_widgets.dart';
 import 'package:ecommerce/registration_login/signup.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
+import 'secrets.dart';
 
 /// INITIALIZE FIREBASE
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: FirebaseOptions(
-      apiKey: dotenv.env['FIREBASE_API_KEY']!,
-      appId: dotenv.env['FIREBASE_APP_ID']!,
-      messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID']!,
-      projectId: dotenv.env['FIREBASE_PROJECT_ID']!,),
-
+    options: const FirebaseOptions(
+        apiKey: Secrets.FIREBASE_API_KEY,
+        appId: Secrets.FIREBASE_APP_ID,
+        messagingSenderId: Secrets.FIREBASE_MESSAGING_SENDER_ID,
+        projectId: Secrets.FIREBASE_PROJECT_ID)
   );
-  try {
-  await dotenv.load(fileName: ".env"); // Load environment variables
-} catch (e) {
-  throw Exception('Error loading .env file: $e'); // Print error if any
-}
   runApp(const MyApp());
-}
-
-/// ACCESSING VARIABLES THROUGHOUT THE APP
-void someFunction() {
-  print(dotenv.env['FIREBASE_API_KEY']);
-  print(dotenv.env['FIREBASE_APP_ID']);
-  print(dotenv.env['FIREBASE_MESSAGING_SENDER_ID']);
-  print(dotenv.env['FIREBASE_PROJECT_ID']);
 }
 
 class MyApp extends StatelessWidget {
