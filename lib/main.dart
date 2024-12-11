14427ef9eb4c9b6e3d64e3887260492682462142
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2024-12-11 10:24:28 +0100

    added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'package:ecommerce/custom_widgets/custom_widgets.dart';
 import 'package:ecommerce/registration_login/login.dart';
-import 'package:ecommerce/registration_login/signup.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
